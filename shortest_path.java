803a97e951a949b240f6a957245873f211be3c90
Sanika <sanikajoshi8903@gmail.com>
2024-03-10 23:41:10 +0530

    initial commit

---
+++ shortest_path.java
@@ -0,0 +1,34 @@
+package strings;
+import java.util.Scanner;
+
+public class shortest_path {
+
+    public static float path(String str){
+        int x = 0;
+        int y =0;
+        for(int i=0;i<str.length();i++){
+            if(str.charAt(i) == 'N'){
+                y++;
+            }
+            else if(str.charAt(i) == 'S'){
+                y--;
+            }
+            else if(str.charAt(i) == 'E'){
+                x++;
+            }
+            else if(str.charAt(i) == 'W'){
+                x--;
+            }
+        }
+        int X2 = x*x;
+        int Y2 = y*y;
+       
+        return (float)Math.sqrt(X2 + Y2);
+    }
+
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        String str = sc.nextLine();
+        System.out.println(path(str));
+    }
+}
